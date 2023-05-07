f45e081d5d9ac0e9320f101ef115060afe854d5f
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-05-08 01:59:40 +0800

    Make data generator bug-free

--- Simple-Realtime-Broadcasting-Server/src/RandomDataGenerator.java
+++ Simple-Realtime-Broadcasting-Server/src/RandomDataGenerator.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
@@ -21,19 +22,24 @@ public class RandomDataGenerator implements Runnable {
     /**
      * Write generated random integers to "server_data.txt"
      */
-    private BufferedWriter bufferedWriter;
+    private FileWriter fileWriter;
 
     /**
      * The default file path of server data file
      */
     private String filePath = "server_data.txt";
 
+    /**
+     * The date formatter which converts the current time to a more precise one
+     */
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
     public RandomDataGenerator(int range) {
         this.range = range;
         this.randomIntegerGenerator = new Random();
         while (true) {
             try {
-                this.bufferedWriter = new BufferedWriter(new FileWriter(this.filePath, true));
+                this.fileWriter = new FileWriter(this.filePath, true);
                 break;
             } catch (IOException e) {
                 e.printStackTrace();
@@ -50,25 +56,19 @@ public class RandomDataGenerator implements Runnable {
         while (true) {
             // generate the random int
             randomInteger = this.randomIntegerGenerator.nextInt(this.range);
-            // sleep 250ms
-            try {
-                Thread.sleep(250);
-            } catch (InterruptedException e) {
-                System.out.println("The data generation thread was interrupted while sleeping");
-                // Re-interrupt the thread to preserve the interruption status
-                Thread.currentThread().interrupt();
-            }
             // prepare data
-            String line = "[" + new Date() + "]" + "::" + randomInteger;
+            String formattedTime = this.dateFormatter.format(new Date(System.currentTimeMillis()));
+            String line = "[" + formattedTime + "]" + "::" + randomInteger;
             // save to the file
             try {
-                bufferedWriter.write(line);
+                fileWriter.write(line + "\n");
+                fileWriter.flush();
             } catch (IOException e) {
                 e.printStackTrace();
                 System.out.println("An error occurred when attempting to write to the file \"" + filePath + "\"\n Retrying with the same path...");
                 while (true) {
                     try {
-                        this.bufferedWriter = new BufferedWriter(new FileWriter(this.filePath, true));
+                        this.fileWriter = new FileWriter(this.filePath, true);
                         break;
                     } catch (IOException ee) {
                         ee.printStackTrace();
@@ -77,6 +77,14 @@ public class RandomDataGenerator implements Runnable {
                         filePath = scanner.nextLine();
                     }
                 }
+            }
+            // sleep 250ms
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                System.out.println("The data generation thread was interrupted while sleeping");
+                // Re-interrupt the thread to preserve the interruption status
+                Thread.currentThread().interrupt();
             }
         }
     }
--- Simple-Realtime-Broadcasting-Server/src/SimpleRealtimeBroadcastingServer.java
+++ Simple-Realtime-Broadcasting-Server/src/SimpleRealtimeBroadcastingServer.java
@@ -1,8 +1,9 @@
 public class SimpleRealtimeBroadcastingServer {
 
-
-
     public static void main(String args[]) {
-
+        Thread dataGeneratorThread = new Thread(new RandomDataGenerator(100));
+        dataGeneratorThread.setDaemon(true);
+        dataGeneratorThread.start();
+        
     }
 }
